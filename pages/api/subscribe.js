407633234d436cbac964c2abe2d2ca2eed19eb84
caiodomingues <caiodss99@hotmail.com>
2020-09-11 12:40:28 -0300

    feat: base newsletter

---
+++ pages/api/subscribe.js
@@ -0,0 +1,38 @@
+import { MongoClient } from "mongodb";
+import url from "url";
+
+let cachedDb = null;
+
+async function connectToDatabase(uri) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const dbName = url.parse(uri).pathname.substr(1);
+
+  const db = client.db(dbName);
+
+  cachedDb = db;
+
+  return db;
+}
+
+export default async (request, response) => {
+  const { email } = request.body;
+
+  const db = await connectToDatabase(process.env.MONGODB_URI);
+
+  const collection = db.collection("subscribers");
+
+  await collection.insertOne({
+    email,
+    subscribedAt: new Date(),
+  });
+
+  return response.status(201).json({ ok: true });
+};
---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === "" || email.length < 3) return;
+
+    axios.post("/api/subscribe", { email });
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>VemProFut</title>
+        <link rel="icon" href="/logo.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h3>VemProFut</h3>
+          <p>Quer ficar por dentro das novidades? Assine a nossa newsletter</p>
+          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Seu melhor e-mail"
+              className={styles.input}
+            />
+            <button type="submit" className={styles.button}>
+              Assinar
+            </button>
+          </form>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <img src="/logo.png" alt="Thoca Logo" className={styles.logo} />
+      </footer>
+    </div>
+  );
+}
